8b2f4d121bf007ebe923d7b900205b43d5b1cbb2
constancees <163030779+constancees@users.noreply.github.com>
2025-12-16 17:06:51 +0100

    Commented definitifpasparfait.py
    added necessary comments for a clear understanding of the code (as well as translating some of the remaining french text)

--- simulation_pkg/simulation_pkg/definitifpasparfait.py
+++ simulation_pkg/simulation_pkg/definitifpasparfait.py
@@ -7,13 +7,15 @@ from geometry_msgs.msg import Twist
 class MySimulationNode(Node):
     
     def __init__(self):
-        super().__init__('simulation_node') ## ne pas supprimer, permet au truc davoir acces aux fonctionnalites ros2
-        self.subscriber_ = self.create_subscription(
+        super().__init__('simulation_node') # important, allows node to access ROS 2 functions
+        self.subscriber_ = self.create_subscription( # receives scanner data
             LaserScan,
             '/scan',
             self.scan_callback,
             10)
         self.cmd_vel_publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
+        
+        # declaring necessary constants and initial state of the robot
         self.Kp = 0.40
         self.max_speed = 0.8
         self.max_turn_speed = 0.6
@@ -26,15 +28,18 @@ class MySimulationNode(Node):
     def scan_callback(self, msg):
         ranges = msg.ranges
 
-        n = len(ranges)
+        n = len(ranges) # guarantees that we are using the correct values for our application
+        # regardless of the amount of values the sensor captures
         front = msg.ranges[n//2]
         left = msg.ranges[int(n*3/4)]
         right = msg.ranges[int(n*1/4)]
 
+        # prints the current sensor values, useful for troubleshooting and tuning
         self.get_logger().info("left : " + str(left))
         self.get_logger().info("right : " + str(right))
         self.get_logger().info("front : " + str(front))
 
+        # FSM : goes into the behaviour decided in the previous loop / initialisation
         if self.state == "FORWARD":
             self.forward_behavior(front, left, right)
         elif self.state == "TURN":
@@ -54,7 +59,7 @@ class MySimulationNode(Node):
             self.turn_direction = "LEFT" if right > left else "RIGHT"
             return
 
-        # Centering controller
+        # centering controller
         error = left - right
         angular = self.Kp * error
 
@@ -66,21 +71,21 @@ class MySimulationNode(Node):
         self.linear = max(0.2, self.linear)
 
         self.move_robot(self.linear, angular)
-        self.get_logger().info("devant")
+        self.get_logger().info("devant") # troubleshooting, announces state we are in
 
     # -------------------------
     # TURN: fixed rotation
     # -------------------------
     def turn_behavior(self, front,left,right):
 
-        # Apply fixed angular speed
+        # apply fixed angular speed
         angular = 1.1 if self.turn_direction == "LEFT" else -1.1
 
-        # No linear motion during turn
+        # no linear motion during turn
         self.move_robot(0.0, angular)
         self.get_logger().info("tourne")
 
-        # Detect when new corridor is visible
+        # detect when new corridor is visible
         if front > 2.5:
             self.state = "REALIGN"
 
@@ -102,12 +107,12 @@ class MySimulationNode(Node):
 
         self.move_robot(self.linear,angular)
 
+        # if the next turn is before the end of this state, allows robot to switch state
         if front < self.turn_trigger:
             self.state = "TURN"
             # turn toward the more open side
             self.turn_direction = "LEFT" if right > left else "RIGHT"
             return
-
 
     def move_robot(self, linear, angular):
         msg = Twist()
@@ -115,10 +120,8 @@ class MySimulationNode(Node):
         msg.angular.z = angular
         self.cmd_vel_publisher_.publish(msg)
 
-
-
 def main(args=None):
-    rclpy.init(args=args) ## laisser ca tjrs en premier
+    rclpy.init(args=args) ## always first
     node = MySimulationNode()
     rclpy.spin(node)
     rclpy.shutdown()
